c83dbff150673855d4cd1ee86083c351bd7f0d3c
Artem Iakovlev <iakovlev.artem.a@gmail.com>
2024-02-24 16:24:08 +0400

    Create path alies

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path, { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(path.resolve(__dirname, "src")),
+    },
+  },
+});
